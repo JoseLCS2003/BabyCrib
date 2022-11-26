8f5293d0f40c939232573cbbb01cef05187a3cbe
Jose Luis Carrera Soto <211170053@uttcampus.edu.mx>
2022-11-25 22:18:26 -0600

    subir proyecto

---
+++ app/src/main/java/com/example/babycrib/inicio.java
@@ -0,0 +1,32 @@
+package com.example.babycrib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
+
+public class inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+        splash();
+    }
+    public void splash(){
+        CountDownTimer t =new CountDownTimer(5000,1000) {
+            @Override
+            public void onTick(long l) {
+                Long d = l / 1000;
+            }
+
+            @Override
+            public void onFinish() {
+                Intent i=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        }.start();
+    }
+}
