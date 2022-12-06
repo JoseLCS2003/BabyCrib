f85f78ec8b3e7dcb5f9511127a6e02afecbc306c
Jose Luis Carrera Soto <211170053@uttcampus.edu.mx>
2022-12-06 09:21:10 -0600

    vista Cuna

---
+++ app/src/main/java/com/example/babycrib/Cuna.java
@@ -0,0 +1,14 @@
+package com.example.babycrib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cuna extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuna);
+    }
+}
