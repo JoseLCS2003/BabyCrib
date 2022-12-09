cbcc05ac7e739117a3f4056b86333aace949233b
Jose Luis Carrera Soto <211170053@uttcampus.edu.mx>
2022-12-08 21:48:51 -0600

    agregar vista de Verificacion

---
+++ app/src/main/java/com/example/babycrib/verificarCodigo.java
@@ -0,0 +1,14 @@
+package com.example.babycrib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class verificarCodigo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verificar_codigo);
+    }
+}
